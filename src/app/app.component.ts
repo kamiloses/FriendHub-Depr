7120f397246d74c55f913fb19e4e0e08eb28a055
kamil <kamil.kurzaj04@gmail.com>
2024-11-06 18:37:12 +0100

    Simple sidebars

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {HeaderComponent} from './header/header.component';
+import {LeftSidebarComponent} from './left-sidebar/left-sidebar.component';
+import {RightSidebarComponent} from './right-sidebar/right-sidebar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, LeftSidebarComponent, RightSidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

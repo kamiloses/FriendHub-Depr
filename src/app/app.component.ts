d547642d91e90e2645407d65f51903c7dea7b206
kamil <kamil.kurzaj04@gmail.com>
2024-11-07 09:27:59 +0100

    added post-list component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import {HeaderComponent} from './header/header.component';
 import {LeftSidebarComponent} from './left-sidebar/left-sidebar.component';
 import {RightSidebarComponent} from './right-sidebar/right-sidebar.component';
+import {PostsListComponent} from './posts-list/posts-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, LeftSidebarComponent, RightSidebarComponent],
+  imports: [RouterOutlet, HeaderComponent, LeftSidebarComponent, RightSidebarComponent, PostsListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

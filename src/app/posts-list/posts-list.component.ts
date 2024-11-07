7d98b17b91dc3025bc0f2272b120155f330f03f9
kamil <kamil.kurzaj04@gmail.com>
2024-11-07 11:30:17 +0100

    add post component

--- src/app/posts-list/posts-list.component.ts
+++ src/app/posts-list/posts-list.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {PostComponent} from './post/post.component';
 
 @Component({
   selector: 'app-posts-list',
   standalone: true,
-  imports: [],
+  imports: [
+    PostComponent
+  ],
   templateUrl: './posts-list.component.html',
   styleUrl: './posts-list.component.css'
 })

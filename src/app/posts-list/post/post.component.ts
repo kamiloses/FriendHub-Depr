e585059d29043a90abce13ba05bebf8a446a281a
kamil <kamil.kurzaj04@gmail.com>
2024-11-07 13:30:24 +0100

    added view of posts

--- src/app/posts-list/post/post.component.ts
+++ src/app/posts-list/post/post.component.ts
@@ -9,4 +9,35 @@ import { Component } from '@angular/core';
 })
 export class PostComponent {
 
+
+
+  post=[{
+     id:null,
+     userId:1,
+    content:"  Lorem Ipsum is simply dummy text of the printing and typesetting industry.\n" +
+    "    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,\n" +
+    "    when an unknown printer took a galley of type and scrambled it to make a type\n" +
+    "    specimen book. It has survived not only five centuries, but also the leap into\n" +
+    "    electronic."},{ id:null,
+  userId:1,
+  content:"  Lorem Ipsum is simply dummy text of the printing and typesetting industry.\n" +
+  "    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,\n" +
+  "    when an unknown printer took a galley of type and scrambled it to make a type\n" +
+  "    specimen book. It has survived not only five centuries, but also the leap into\n" +
+  "    electronic."},{
+    id:null,
+  userId:1,
+  content:"  Lorem Ipsum is simply dummy text of the printing and typesetting industry.\n" +
+  "    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,\n" +
+  "    when an unknown printer took a galley of type and scrambled it to make a type\n" +
+  "    specimen book. It has survived not only five centuries, but also the leap into\n" +
+  "    electronic."},
+
+
+
+
+  ]
+
+
+
 }

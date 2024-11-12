4a33e28f9cb573853acae3b3763953fb1155db17
kamil <kamil.kurzaj04@gmail.com>
2024-11-12 10:04:32 +0100

    added routing after clicing post

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import {PostComponent} from './posts-list/post/post.component';
+import {PostDetailComponent} from './posts-list/post/post-detail/post-detail.component';
+import {PostsListComponent} from './posts-list/posts-list.component';
 
 export const routes: Routes = [
-  { path: 'post/:id', component: PostComponent }]
+
+  { path: '', component: PostsListComponent },
+  { path: 'post/:id', component: PostDetailComponent }]
 

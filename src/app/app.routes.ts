f96d4d4c23fa95cb1be05834a9430a73829562a8
kamil <kamil.kurzaj04@gmail.com>
2024-11-12 07:02:42 +0100

    created post-detal component and added routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import {PostComponent} from './posts-list/post/post.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'post/:id', component: PostComponent }]
+
--- src/app/posts-list/post/post.component.ts
+++ src/app/posts-list/post/post.component.ts
@@ -1,10 +1,13 @@
 import {Component, Input} from '@angular/core';
 import {Post} from '../../models/post-model';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-post',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.css']
 })

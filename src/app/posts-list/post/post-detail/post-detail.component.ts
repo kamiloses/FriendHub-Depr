ef0c8d12f3c20c2c17e709c72799ceeb4a220073
kamil <kamil.kurzaj04@gmail.com>
2024-11-14 07:58:26 +0100

    added http fetch method in post-detail ts

--- src/app/posts-list/post/post-detail/post-detail.component.ts
+++ src/app/posts-list/post/post-detail/post-detail.component.ts
@@ -1,5 +1,10 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {CommentsComponent} from './comments/comments.component';
+import {HttpClient} from '@angular/common/http';
+import {ActivatedRoute, Router} from '@angular/router';
+import {Post} from '../../../models/post-model';
+import {Subscription} from 'rxjs';
+
 
 @Component({
   selector: 'app-post-detail',
@@ -10,6 +15,48 @@ import {CommentsComponent} from './comments/comments.component';
   templateUrl: './post-detail.component.html',
   styleUrl: './post-detail.component.css'
 })
-export class PostDetailComponent {
+export class PostDetailComponent implements OnInit,OnDestroy {
 
+
+  post!: Post
+
+
+  private subscription: Subscription | null = null;
+
+  constructor(private httpClient: HttpClient, private router: Router, private activatedRoute: ActivatedRoute) {
+    this.router.events.subscribe(() => {
+      this.modifyRouter();
+    });
+  }
+
+  currentRoute!: string;
+
+  modifyRouter() {
+
+    this.currentRoute = this.router.url.substring(this.router.url.lastIndexOf("/") + 1);
+    console.log("router :"+this.currentRoute)
+    console.log("hey"+this.router.url.substring(this.router.url.lastIndexOf("/") + 1))
+  }
+
+
+  ngOnInit(): void {
+
+    this.subscription = this.httpClient.get<Post>("http://localhost:8080/api/posts/"+this.currentRoute).subscribe({
+      next: (data) => {
+        console.log(data);
+        this.post = data;
+      },
+      error: (error) => {
+        console.error('Error while downloading data:', error);
+      }
+    });
+
+  }
+
+  ngOnDestroy(): void {
+
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
 }

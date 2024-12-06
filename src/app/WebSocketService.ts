1281b8ae769e22a41b4896d884f7a5e01937dd64
kamil <kamil.kurzaj04@gmail.com>
2024-12-06 09:34:32 +0100

    added subscription to backend websockets

--- src/app/WebSocketService.ts
+++ src/app/WebSocketService.ts
@@ -8,20 +8,45 @@ import * as Stomp from 'stompjs';
 export class WebSocketService {
 
   private stompClient: any;
-  private webSocketEndpoint: string = 'http://localhost:8081/ws';
+  //private webSocketEndpoint: string = 'http://localhost:8084/ws';
 
   connect(loggedUser:string |null): void {
-    let ws = new SockJS(this.webSocketEndpoint);
+    let ws = new SockJS('http://localhost:8084/ws');
     this.stompClient = Stomp.over(ws);
+
 
     this.stompClient.connect(
       { 'username': loggedUser },
       (frame: string) => {
         console.log('Connected to STOMP: ' + frame);
+        this.stompClient.send('/app/chat.activeFriends', {},loggedUser);
+
+
+
+        this.stompClient.subscribe('/topic/public/friendsOnline', (message: any) => {
+          this.onFriendsOnlineUpdate(message);
+        });
+
       },
       (error: string) => {
         console.error('Connection error: ', error);
       }
     );
   }
+
+
+
+
+
+  private onFriendsOnlineUpdate(message: any): void {
+    const friendsOnline: string[] = JSON.parse(message.body);
+    console.log('Received friends online data: ', friendsOnline);
+
+    this.updateFriendsOnlineList(friendsOnline);
+  }
+
+  private updateFriendsOnlineList(friendsOnline: string[]): void {
+    console.log('Friends Online:', friendsOnline);
+  }
+
 }

9c5fe7ab18b0a69fac5f342a2c6f66ae1701f6f4
kamil <kamil.kurzaj04@gmail.com>
2025-01-15 07:28:57 +0100

    created userProfile component

---
+++ src/app/user-profile/user-profile.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {PostsListComponent} from '../posts-list/posts-list.component';
+
+@Component({
+  selector: 'app-user-profile',
+  standalone: true,
+  imports: [
+    PostsListComponent
+  ],
+  templateUrl: './user-profile.component.html',
+  styleUrl: './user-profile.component.css'
+})
+export class UserProfileComponent {
+
+}

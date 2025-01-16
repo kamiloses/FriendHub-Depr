04434f20b8d40836dda3ef871f163d7b74454474
kamil <kamil.kurzaj04@gmail.com>
2025-01-16 10:55:53 +0100

    added opportunity to retweet posts and undoRetweet

--- src/app/posts-list/post/post.component.ts
+++ src/app/posts-list/post/post.component.ts
@@ -1,6 +1,8 @@
 import {Component, Input} from '@angular/core';
 import {Post} from '../../models/post-model';
 import {RouterLink} from '@angular/router';
+import {HttpClient} from '@angular/common/http';
+import {User} from '../../models/user-model';
 
 @Component({
   selector: 'app-post',
@@ -11,10 +13,43 @@ import {RouterLink} from '@angular/router';
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.css']
 })
-export class PostComponent  {
+export class PostComponent {
 
-  @Input({required:true}) postDetails!:Post
+  @Input({required: true}) postDetails!: Post
 
+  constructor(private httpClient: HttpClient) {
+  }
 
   protected readonly JSON = JSON;
+
+  @Input() loggedUserUsername!: string;
+
+
+  retweet(event:MouseEvent ): void {
+  event.stopPropagation();
+
+
+      console.log("RETWEETUJE"+JSON.stringify(this.postDetails)+ "USERNAME"+this.loggedUserUsername)
+
+      this.httpClient.post<void>(`http://localhost:8080/api/retweet?postId=${this.postDetails.id}&username=${this.loggedUserUsername}`, null)
+        .subscribe(() => {
+          window.location.reload();
+        });
+
+  }
+  undoRetweet(event:MouseEvent):void{
+    event.stopPropagation();
+    console.log("COFAM"+JSON.stringify(this.postDetails) + "USERNAME"+this.loggedUserUsername)
+
+    this.httpClient.delete<void>(`http://localhost:8080/api/retweet?postId=${this.postDetails.id}&username=${this.loggedUserUsername}`)
+      .subscribe(() => {
+        window.location.reload();
+      });
+
+  }
+
+
+
+  protected readonly event = event;
+
 }

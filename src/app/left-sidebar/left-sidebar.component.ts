d96d6c75ee95444a045b52aec30d424da6f1f071
kamil <kamil.kurzaj04@gmail.com>
2025-03-05 11:53:51 +0100

    changed left-sidebar component

--- src/app/left-sidebar/left-sidebar.component.ts
+++ src/app/left-sidebar/left-sidebar.component.ts
@@ -1,18 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {GlobalEnvironmentVariables} from '../models/globalEnvironmentVariables';
-import {Router} from '@angular/router';
+import {Router, RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-left-sidebar',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './left-sidebar.component.html',
   styleUrl: './left-sidebar.component.css'
 })
-export class LeftSidebarComponent {
+export class LeftSidebarComponent implements OnInit {
 
 
-  constructor( private globalEnvironmentVariables: GlobalEnvironmentVariables,private router: Router) {}
+  constructor(private globalEnvironmentVariables: GlobalEnvironmentVariables, private router: Router) {
+  }
+
+  protected myUsername!: string | null;
+
+  ngOnInit() {
+    this.myUsername = sessionStorage.getItem('username')||"fail"
+
+  }
 
 
   logout(): void {
@@ -23,14 +33,10 @@ export class LeftSidebarComponent {
     this.globalEnvironmentVariables.setGlobalToken(null);
     this.globalEnvironmentVariables.setGlobalUsername(null);
     this.globalEnvironmentVariables.setGlobalSession(false);
-    this.router.navigate(['/login'])
+    
+    this.router.navigate(['/login']).then(r => console.log("successfully logged out."));
+
   }
-
-
-  protected getUsername=sessionStorage.getItem('username')
-
-
-
 
 
 }
